ccf68afa72adcca4c260f4ee29716ad2d1fb0a79
XchanBik <xchanbik@gmail.com>
2025-05-23 19:48:52 +0200

    Push base

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,21 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
-    vue(),
     electron([
       {
-        entry: 'src/main/index.ts',
-        vite: {
-          build: {
-            outDir: 'dist-electron/main',
-          },
+        // Main-Process entry file of the Electron App.
+        entry: 'electron/main.ts',
+      },
+      {
+        entry: 'electron/preload.ts',
+        onstart(options) {
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
+          // instead of restarting the entire Electron App.
+          options.reload()
         },
       },
     ]),
-    renderer(),
   ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src/renderer'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-}) 
+})
