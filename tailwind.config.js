763b2f05b67e2475f8b4132f22dc1a42bc1da6b4
XchanBik <xchanbik@gmail.com>
2025-05-23 03:20:46 +0200

    Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ export default {
       colors: {
         primary: '#42b983',
         secondary: '#2c3e50',
+      },
+      borderRadius: {
+        DEFAULT: '0.375rem',
+        'sm': '0.125rem',
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        'xl': '0.75rem',
+        '2xl': '1rem',
+        '3xl': '1.5rem',
+        'full': '9999px',
       }
     },
   },
