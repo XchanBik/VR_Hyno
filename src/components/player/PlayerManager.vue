5bfc9314229cf96796475ef6056494fc8681f079
XchanBik <xchanbik@gmail.com>
2025-05-24 14:38:12 +0200

    Add ManagerSTep

--- src/components/player/PlayerManager.vue
+++ src/components/player/PlayerManager.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { useNavigationStore } from '@/store/navigation'
 import PlaylistManager from '@/components/player/playlist/PlaylistManager.vue'
-import PlaylistEditor from '@/components/player/playlist/PlaylistEditor.vue'
-import PlaylistPlayer from '@/components/player/playlist/PlaylistPlayer.vue'
 
 const nav = useNavigationStore()
 </script>
@@ -10,9 +8,7 @@ const nav = useNavigationStore()
 <template>
   <div class="w-full h-full bg-gradient-to-br from-brand-100 via-brand-50 to-brand-200 rounded-xl shadow-lg">
     <div class="p-4 overflow-y-auto h-full">
-      <PlaylistManager v-if="nav.path[1] === 'playlist' && nav.path[2] === 'list'" />
-      <PlaylistEditor v-else-if="nav.path[1] === 'playlist' && nav.path[2] === 'edit'"/>
-      <PlaylistPlayer v-else-if="nav.path[1] === 'playlist' && nav.path[2] === 'player'"/>
+      <PlaylistManager v-if="nav.path[1] === 'playlist'" />
     </div>
   </div>
 </template>
